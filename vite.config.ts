e64b62ca6b5a498c63948494656606492cf6bdc6
夏空alpha <1707075217@qq.com>
2026-02-10 12:45:08 +0800

    csp

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,32 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import csp from 'vite-plugin-csp'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+    csp(
+      {
+        'default-src': ["self"],
+        'script-src': ["self", "unsafe-inline"],
+        'style-src': ["self", "unsafe-inline"],
+        'img-src': ["self", "data:", "https:"],
+        'connect-src': ["self", "https://api.example.com"],
+        'font-src': ["self", "https://fonts.gstatic.com"],
+        'object-src': ["none"],
+        'frame-ancestors': ["none"],
+      },
+      {
+        enabled: true,
+        onDev: "skip",
+        hashingMethod: 'sha256',
+        nonceEnabled: {
+          'script-src': true,
+          'style-src': true
+        }
+      }
+    )
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
