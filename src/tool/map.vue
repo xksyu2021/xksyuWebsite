c18c768ec943614adf77775bb56ec9e9cd02eade
夏空alpha <1707075217@qq.com>
2026-04-27 19:38:09 +0800

    localstorage for map.vue

--- src/tool/map.vue
+++ src/tool/map.vue
@@ -1,12 +1,31 @@
 <script setup lang="ts">
-import {computed, onMounted, onUnmounted, ref} from "vue";
-const x_r = ref(false)
-const z_r = ref(false)
-const xz_r = ref(false)
-const imgSize = ref(100)
-const posSizeX = ref(100)
-const posSizeZ = ref(100)
-const showFloat = ref(false);
+import {computed, onMounted, onUnmounted, ref, watch} from "vue";
+
+const showFloat = ref(false)
+
+//saved data
+interface dataIf{
+  x_r:Boolean,z_r:Boolean,xz_r:Boolean,imgSize:number,posSizeX:number,posSizeZ:number
+}
+const rawData = localStorage.getItem("saved")
+const data = ref<dataIf>(rawData?JSON.parse(rawData):
+    {x_r: false, z_r: false, xz_r: false, imgSize: 100, posSizeX: 100, posSizeZ: 100}
+)
+watch(data,(cur)=>{
+  localStorage.setItem("saved",JSON.stringify(cur))
+},{deep:true})
+//------
+
+const x = ref(0) // currentPos
+const z = ref(0) // currentPos
+
+// following used for auto-get-posSize
+const x_fix = ref(0) // zeroPoint
+const z_fix = ref(0) // zeroPoint
+const x_fix_adv = ref(1) // refPoint
+const z_fix_adv = ref(1) // refPoint
+const x_fix_adv_std = ref(1024) // refPointStd
+const z_fix_adv_std = ref(1024) // refPointStd
 
 const imageUrl = ref<string>("");
 const imgInput = ref<HTMLInputElement | null>(null);
@@ -25,15 +44,6 @@ const fileH = (e: Event) => {
     }
   }
 }
-
-const x = ref(0);
-const z = ref(0);
-const x_fix = ref(0);
-const z_fix = ref(0);
-const x_fix_adv = ref(0);
-const z_fix_adv = ref(0);
-const x_fix_adv_std = ref(1024);
-const z_fix_adv_std = ref(1024);
 
 const fixMouse = (e :Event) => {
   e.stopPropagation();
@@ -57,8 +67,8 @@ const fixMouseAdv = (e :Event) => {
   window.addEventListener('click', getClick, { once: true });
 }
 const fixMouseAdvStd = () => {
-  posSizeX.value = x_fix_adv_std.value/(x_fix_adv.value-x_fix.value) *100;
-  posSizeZ.value = z_fix_adv_std.value/(z_fix_adv.value-z_fix.value) *100;
+  data.value.posSizeX = x_fix_adv_std.value/(x_fix_adv.value-x_fix.value) *100;
+  data.value.posSizeZ = z_fix_adv_std.value/(z_fix_adv.value-z_fix.value) *100;
 }
 
 const updateMouse = (e: { pageX: number; pageY: number; }) => {
@@ -83,52 +93,54 @@ const float = computed(() => {
 </script>
 
 <template>
-  <div class="overlay no-select">
-    <div class="card imgCard"
-         @mouseenter="showFloat = true" @mouseleave="showFloat = false">
-      <img class="map" v-if="imageUrl" :src="imageUrl" alt="地图文件"
-        :style="{width: imgSize + '%'}"/>
-    </div>
-    <div class="card opCard">
-      <div class="row advRow">
-        <div class="button buttonA"
-             @click="triggerUpload">导入图片
-          <input type="file" ref="imgInput" accept="image/*"
-                 style="display: none"
-                 @change="fileH" >
+  <div>
+    <div class="overlay no-select">
+      <div class="card imgCard"
+           @mouseenter="showFloat = true" @mouseleave="showFloat = false">
+        <img class="map" v-if="imageUrl" :src="imageUrl" alt="地图文件"
+          :style="{width: data.imgSize + '%'}"/>
+      </div>
+      <div class="card opCard">
+        <div class="row advRow">
+          <div class="button buttonA"
+               @click="triggerUpload">导入图片
+            <input type="file" ref="imgInput" accept="image/*"
+                   style="display: none"
+                   @change="fileH" >
+          </div>
+          <div class="button buttonB" @click="fixMouse">标记原点</div>
         </div>
-        <div class="button buttonB" @click="fixMouse">标记原点</div>
+        <div>图片放缩 <input type="number" class="field" v-model="data.imgSize"
+                             min="0.001" max="100000" step="10"/> %</div>
+        <div>标度放缩 <input type="number" class="field"
+                             :value="data.posSizeX"
+                             @input="e => { data.posSizeX = data.posSizeZ = Number((e.target as HTMLInputElement).value) }"
+                             min="0.001" max="100000" step="10"/> %</div>
+        <div class="row">
+          <div class="row"><input type="checkbox" id="check_x_r" v-model="data.x_r" />x轴反转</div>
+          <div class="row"><input type="checkbox" id="check_y_r" v-model="data.z_r" />z轴反转</div>
+          <div class="row"><input type="checkbox" id="check_y_r" v-model="data.xz_r" />xz轴交换</div>
+        </div>
       </div>
-      <div>图片放缩 <input type="number" class="field" v-model="imgSize"
-                           min="0.001" max="100000" step="10"/> %</div>
-      <div>标度放缩 <input type="number" class="field"
-                           :value="posSizeX"
-                           @input="e => { posSizeX = posSizeZ = Number((e.target as HTMLInputElement).value) }"
-                           min="0.001" max="100000" step="10"/> %</div>
-      <div class="row">
-        <div class="row"><input type="checkbox" id="check_x_r" v-model="x_r" />x轴反转</div>
-        <div class="row"><input type="checkbox" id="check_y_r" v-model="z_r" />z轴反转</div>
-        <div class="row"><input type="checkbox" id="check_y_r" v-model="xz_r" />xz轴交换</div>
+      <div class="card opCard">
+        <div class="row advRow">
+          选择坐标修正标度 <div class="button buttonB" @click="fixMouseAdv">标记参考点</div>
+        </div>
+        此项会自动更新左侧工具中的“标度放缩”。<br>
+        修正点坐标：
+        <div class="row">
+          <div> x&nbsp;=&nbsp; <input type="number" class="field" v-model="x_fix_adv_std"/> &nbsp;</div>
+          <div> z&nbsp;=&nbsp; <input type="number" class="field" v-model="z_fix_adv_std"/> </div>
+          <div class="button buttonA" style="margin-left: 20px"
+               @click="fixMouseAdvStd">确定</div>
+        </div>
       </div>
     </div>
-    <div class="card opCard">
-      <div class="row advRow">
-        选择坐标修正标度 <div class="button buttonB" @click="fixMouseAdv">标记参考点</div>
-      </div>
-      此项会自动更新左侧工具中的“标度放缩”。<br>
-      修正点坐标：
-      <div class="row">
-        <div> x&nbsp;=&nbsp; <input type="number" class="field" v-model="x_fix_adv_std"/> &nbsp;</div>
-        <div> z&nbsp;=&nbsp; <input type="number" class="field" v-model="z_fix_adv_std"/> </div>
-        <div class="button buttonA" style="margin-left: 20px"
-             @click="fixMouseAdvStd">确定</div>
-      </div>
-    </div>
-  </div>
 
-  <div class="float no-select" :style="float" v-show="showFloat" >
-    {{xz_r?"z":"x"}} = {{ Math.trunc((x - x_fix) * posSizeX / 100) * (x_r?-1:1) }} ,
-    {{xz_r?"x":"z"}} = {{ Math.trunc((z - z_fix) * posSizeZ / 100) * (z_r?-1:1) }}
+    <div class="float no-select" :style="float" v-show="showFloat" >
+      {{data.xz_r?"z":"x"}} = {{ Math.trunc((x - x_fix) * data.posSizeX / 100) * (data.x_r?-1:1) }} ,
+      {{data.xz_r?"x":"z"}} = {{ Math.trunc((z - z_fix) * data.posSizeZ / 100) * (data.z_r?-1:1) }}
+    </div>
   </div>
 </template>
 
